aef79dbedbdf3d0d67f5db1f3865f28333ace097
shc0743 <shc0743@outlook.com>
2025-04-19 09:40:56 +0800

    bug: 修复了弹出窗口标题栏文字溢出的问题

--- vapp.esm.js
+++ vapp.esm.js
@@ -153,15 +153,49 @@ setTimeout(async () => {
 
 
 queueMicrotask(() => {
+    const widgets_container = document.createElement('div');
+    widgets_container.dataset.usage = '__CreateDynamicResizableView__';
+    const CSS = new CSSStyleSheet();
+    const css_Text = `
+    widget-caption {
+        display: flex;
+        flex-direction: row;
+    }
+    widget-caption > span {
+        flex: 1;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        margin-right: 0.5em;
+    }
+    [data-usage="__CreateDynamicResizableView__"] {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        inset: 0;
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        padding: 0;
+    }
+    `;
+    CSS.replace(css_Text).then(() => { document.adoptedStyleSheets.push(CSS) }).catch(() => {
+        const style = document.createElement('style');
+        style.innerHTML = css_Text;
+        widgets_container.append(style);
+    });
     globalThis.CreateDynamicResizableView = function (element, title, width, height) {
         const el = document.createElement('resizable-widget');
         el.style.width = Math.min(window.innerWidth, width) + 'px';
         el.style.height = Math.min(window.innerHeight, height) + 'px';
-        document.getElementById('app').append(el);
+        widgets_container.append(el);
 
         const caption = document.createElement('widget-caption');
         caption.slot = 'widget-caption';
-        caption.append(title);
+        const title_container = document.createElement('span');
+        title_container.textContent = title;
+        caption.append(title_container);
         const close_button = document.createElement('button');
         close_button.innerText = 'x';
         close_button.style.float = 'right';
@@ -190,6 +224,8 @@ queueMicrotask(() => {
             CreateDynamicResizableView(frame, 'External Link', 720, 1000);
         });
     });
+
+    document.getElementById('myApp').after(widgets_container);
 });
 
 
